87cceec424ec16ab499764d2e42771977befceea
SeanGau <haca00193@gmail.com>
2020-08-03 23:55:57 +0800

    add example

---
+++ config_example.py
@@ -0,0 +1,20 @@
+import json
+from sqlalchemy import create_engine
+
+SECRET_KEY = ""
+MAIL_SERVER = '127.0.0.1'
+MAIL_PORT = 25
+MAIL_DEFAULT_SENDER = '"大河小溪全民齊督工" <riverwatcher@bambooculture.com>'
+
+mmConfig = {
+	"admin_id": [],
+	"channel_id": "",
+	"test_channel_id": "", #test channel
+	"token": ""
+}
+
+SQLALCHEMY_ENGINE_OPTIONS = {'encoding': 'utf-8', 'json_serializer': lambda obj: obj, 'echo': False}
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'postgresql://:@localhost/riverwatcher'
+gisdb_engine = create_engine('postgresql://:@localhost/gisdb', encoding= 'utf-8', json_serializer= lambda obj: obj)
